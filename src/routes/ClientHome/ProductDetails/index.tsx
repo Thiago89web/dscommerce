d6f05b068bbf81954930ce77f38ec665d19e0c1c
Thiago Santos <virtualves@gmail.com>
2024-12-07 22:45:59 -0400

    ContextCartCount

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -2,16 +2,20 @@ import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
 import "./styles.css";
-import * as productService from "../../../services/product-service";
 import { useNavigate, useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { ProductDTO } from "../../../models/product";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import * as productService from "../../../services/product-service";
+import * as cartService from '../../../services/cart-service';
+import { ContextCartCount } from "../../../utils/context-cart";
 
 export default function ProductDetails() {
   const params = useParams();
 
   const navegate = useNavigate();
+
+  const {setContextCartCount} = useContext(ContextCartCount);
 
   const [product, setProduct] = useState<ProductDTO>();
 
@@ -25,6 +29,14 @@ export default function ProductDetails() {
     })
   }, []);
 
+  function handleBuyClick() {
+    if(product) {
+      cartService.addProduct(product)
+      setContextCartCount(cartService.getCart().items.length);
+      navegate("/cart")
+    }    
+  }
+
   return (
     <main>
       <section id="product-details-section" className="dsc-container">
@@ -34,7 +46,10 @@ export default function ProductDetails() {
         }
 
         <div className="dsc-btn-page-container">
-          <ButtonPrimary text="Comprar" />
+          <div onClick={handleBuyClick}>
+            <ButtonPrimary text="Comprar" />
+          </div>
+          
           <Link to={"/"}>
             <ButtonInverse text="Início" />
           </Link>
