e88c0d39860362fe757206580f879aff33d7a6f3
Thiago Santos <virtualves@gmail.com>
2024-12-08 10:43:02 -0400

    AdminPage Layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import ProductDetails from "./routes/ClientHome/ProductDetails";
 import Cart from "./routes/ClientHome/Cart";
 import { useState } from "react";
 import { ContextCartCount } from "./utils/context-cart";
+import Login from "./routes/ClientHome/Login";
+import Admin from "./routes/Admin";
+import AdminHome from "./routes/Admin/AdminHome";
 
 function App() {
   const [contextCartCount, setContextCartCount] = useState<number>(0);
@@ -21,6 +24,10 @@ function App() {
               element={<ProductDetails />}
             />
             <Route path="cart" element={<Cart />} />
+            <Route path="login" element={<Login />} />
+          </Route>
+          <Route path="/admin/" element={<Admin />}>
+            <Route index element={<AdminHome />} />
           </Route>
           <Route path="*" element={<Navigate to={"/"} />} />
         </Routes>
