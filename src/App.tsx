fc9ee17ae7805c03bbf54ad2ac544b3886fe8c6b
Thiago Santos <virtualves@gmail.com>
2024-12-08 12:54:39 -0400

    Access control route level

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ function App() {
             <Route path="login" element={<Login />} />
           </Route>
           <Route path="/admin/" element={
-            <PrivateRoute>
+            <PrivateRoute roles={['ROLE_ADMIN']}>
               <Admin />
             </PrivateRoute>
           }>
